90b908f57824826ece5f929a2031070f2310517a
Fabian-Robert Stöter <fabian-robert.stoeter@audiolabs-erlangen.de>
2018-05-15 23:44:47 +0200

    change style

--- content/.vuepress/enhanceApp.js
+++ content/.vuepress/enhanceApp.js
@@ -1,6 +1,6 @@
 export default ({ router }) => {
   router.addRoutes([
     { path: '/musdb', redirect: '/datasets/musdb.html' },
-    { path: '/@:foo', redirect: '/references.html#ref-:foo' },
+    { path: '/methods/:foo', redirect: '/references.html:foo' },
   ])
 }

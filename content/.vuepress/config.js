43150f44a2d20671fada04e1b39d0bd69e7ce0dd
Fabian-Robert Stöter <fabian-robert.stoeter@audiolabs-erlangen.de>
2018-05-15 21:48:00 +0200

    adjust config

--- content/.vuepress/config.js
+++ content/.vuepress/config.js
@@ -31,7 +31,7 @@ module.exports = {
     repo: 'sigsep',
     // Defaults to "GitHub"/"GitLab"/"Bitbucket" depending on `themeConfig.repo`
     // if your docs are in a different repo from your main project:
-    docsRepo: 'sigsep/sigsep.github.io',
+    docsRepo: 'sigsep/website',
     // if your docs are not at the root of the repo:
     docsDir: 'content',
     // if your docs are in a specific branch (defaults to 'master'):

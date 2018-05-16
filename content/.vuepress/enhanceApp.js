dec9d322653775671adc2e6a3d99a47843face94
Fabian-Robert Stöter <fabian-robert.stoeter@audiolabs-erlangen.de>
2018-05-17 00:55:10 +0200

    fix redirect

--- content/.vuepress/enhanceApp.js
+++
@@ -1,5 +0,0 @@
-export default ({ router }) => {
-  router.addRoutes([
-    { path: '/musdb', redirect: '/datasets/musdb.html' },
-  ])
-}
---
+++ content/musdb/README.md
@@ -0,0 +1,7 @@
+<script>
+module.exports = {
+  created(){
+    this.$router.push('/datasets/musdb.html')
+  }
+}
+</script>
